2bb36ba9c979498321fcfa4c06c05b62cf05c643
Pavel Diordits <pdiordits@yandex.ru>
2025-05-27 11:38:24 +0300

    v1

--- src/scraper/__init__.py
+++ src/scraper/__init__.py
@@ -1,4 +1,4 @@
-from .scraper import scrape, main
-from .config import SCRAPING_URLS
+from src.scraper.scraper import scrape, main
+from src.scraper.config import SCRAPING_URLS
 
 __all__ = ['scrape', 'main', 'SCRAPING_URLS']
